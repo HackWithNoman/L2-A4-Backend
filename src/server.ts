43d216e349e0c97e088b8a2a2ddb9c1eadb209d1
BitWizCoder <mhmdnoman01@gmail.com>
2026-02-19 16:06:53 +0600

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import app from "./app";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("Skillbridge Backend");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});

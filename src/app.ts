88aaab005f1c1e9f6b36a1ffc0cedb962e46831c
BitWizCoder <mhmdnoman01@gmail.com>
2026-02-23 07:07:28 +0600

    updated auth

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import { authRouter } from "./modules/Auth/auth.router";
+import { tutorRouter } from "./modules/Tutor/tutor.router";
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/api/v1/auth", authRouter);
+
+app.use("/api/v1/tutor", tutorRouter);
 
 export default app;
